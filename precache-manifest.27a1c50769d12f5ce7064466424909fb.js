a28f56aef2d9b3928259ceda46ba461eb720c7fe
Oladehinde Kazeem <larrysnet2001@gmail.com>
2019-11-07 19:04:04 +0100

    Updates

---
+++ precache-manifest.27a1c50769d12f5ce7064466424909fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bdc439133e6a9ba8026fa01f109bdf",
+    "url": "/famap-presentation/index.html"
+  },
+  {
+    "revision": "73cf7f994048b750d71c",
+    "url": "/famap-presentation/static/css/2.ac36bbb9.chunk.css"
+  },
+  {
+    "revision": "80ce8ee8c62854b6bb1f",
+    "url": "/famap-presentation/static/css/main.8283f640.chunk.css"
+  },
+  {
+    "revision": "73cf7f994048b750d71c",
+    "url": "/famap-presentation/static/js/2.aebd8c30.chunk.js"
+  },
+  {
+    "revision": "80ce8ee8c62854b6bb1f",
+    "url": "/famap-presentation/static/js/main.c97c594b.chunk.js"
+  },
+  {
+    "revision": "7e887d07e936e24e379c",
+    "url": "/famap-presentation/static/js/runtime~main.93a5c146.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/famap-presentation/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/famap-presentation/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/famap-presentation/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/famap-presentation/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/famap-presentation/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7c930a60b210c540436cf7bea952bc8a",
+    "url": "/famap-presentation/static/media/logo.7c930a60.png"
+  }
+]);
